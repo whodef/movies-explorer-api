d0f661cb36d5d3832cbe483319d1ed36f53c943f
Tatiana S <tatyana-selyuk@yandex.com>
2021-12-23 23:46:48 +0200

    Fixed cors;

--- app.js
+++ app.js
@@ -24,18 +24,8 @@ app.use(requestLogger);
 
 const corsOptions = {
   origin: true,
-  credentials: true
+  credentials: true,
 }
-
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
 
 app.use(cors(corsOptions));
 
